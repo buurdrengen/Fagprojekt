786e3953ebc2d7b8a63861b8595adedce765b44e
buurdrengen <s203947@student.dtu.dk>
2022-03-09 11:56:58 +0100

    Autocofunction
    En linjes autocorrelation + et helt billede

--- autocofunc.py
+++ autocofunc.py
@@ -12,8 +12,8 @@ threshold = 0.5
 
 image = skimage.io.imread(fname = "20200220_221515.jpg")
 clip = skimage.color.rgb2gray(image[y-margin:y+margin, x-margin:x+margin])
-plt.imshow(clip,cmap = 'gray')
-plt.show()
+# plt.imshow(clip,cmap = 'gray')
+# plt.show()
 
 clip[clip > threshold] = 0
 N = 2*margin
@@ -21,7 +21,26 @@ N = 2*margin
 horizontal = np.sum(clip,axis = 0)/N
 vertical = np.sum(clip,axis = 1)/N
 
+## En linje 
 linje = clip[1,:]
-sm.tsa.acf(linje,nlags = 1999)
-fig = tsaplots.plot_acf(linje,lags = 1999)
+sm.tsa.acf(linje,nlags = 100)
+fig = tsaplots.plot_acf(linje,lags = 100)
+plt.show()
+
+## Et helt billede
+M = np.zeros(2000)
+for i, clips in enumerate(clip):
+    auto = sm.tsa.acf(clips,nlags = 1999)
+    M = M + auto
+    if i %10 == 0: 
+        print(i)
+M = 1/2000*M
+
+plt.figure(1)
+tsaplots.plot_acf(M,lags = 100)
+plt.show()
+
+x1 = np.arange(0, 100)
+y1 = M[0,100]
+plt.plot(x1,y1)
 plt.show()
