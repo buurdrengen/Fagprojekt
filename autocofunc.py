65f512efea5323f13ce2efff8030ad3bc84a74c7
buurdrengen <s203947@student.dtu.dk>
2022-03-16 08:20:03 +0100

    autocofunc update

--- autocofunc.py
+++ autocofunc.py
@@ -1,69 +1,68 @@
-## Autocorrelation function ## 
-import numpy as np
-import skimage.io
-import matplotlib.pyplot as plt 
-import statsmodels.api as sm
-from statsmodels.graphics import tsaplots
+# ## Autocorrelation function ## 
+# import numpy as np
+# import skimage.io
+# import matplotlib.pyplot as plt 
+# import statsmodels.api as sm
+# from statsmodels.graphics import tsaplots
 
-x = 1300 
-y = 3000 
-margin = 1000
-threshold = 0.5 
+# x = 1300 
+# y = 3000 
+# margin = 1000
+# threshold = 0.5 
 
-image = skimage.io.imread(fname = "20200220_221515.jpg")
-clip = skimage.color.rgb2gray(image[y-margin:y+margin, x-margin:x+margin])
-<<<<<<< Updated upstream
-# plt.imshow(clip,cmap = 'gray')
-# plt.show()
-=======
-plt.imshow(clip,cmap = 'gray')
-plt.show()
->>>>>>> Stashed changes
+# image = skimage.io.imread(fname = "20200220_221515.jpg")
+# clip = skimage.color.rgb2gray(image[y-margin:y+margin, x-margin:x+margin])
+# # plt.imshow(clip,cmap = 'gray')
+# # plt.show()
 
-clip[clip > threshold] = 0
-N = 2*margin
+# clip[clip > threshold] = 0
+# N = 2*margin
 
-horizontal = np.sum(clip,axis = 0)/N
-vertical = np.sum(clip,axis = 1)/N
+# horizontal = np.sum(clip,axis = 0)/N
+# vertical = np.sum(clip,axis = 1)/N
 
-<<<<<<< Updated upstream
-# from clipBlur import clipBlur
-# def autoCor()
+from clipBlur import clipBlur
+def autoCor(clipBlur):
+    # Function 
+    # Viser autocorrelation mellem alle pixels i det clipped/blurred billede. 
 
-## En linje 
-linje = clip[1,:]
-sm.tsa.acf(linje,nlags = 100)
-fig = tsaplots.plot_acf(linje,lags = 100)
-plt.show()
+    # Variables
+    # clipped/blurred billede loaded fra clipBlur funktionen.
 
-## Et helt billede
-M = np.zeros(2000)
-for i, clips in enumerate(clip):
-    auto = sm.tsa.acf(clips,nlags = 1999)
-    M = M + auto
-    if i %10 == 0: 
-        print(i)
-M = 1/2000*M
+    # Først importeres relevante pakker, især her statsmodels.
+    import numpy as np
+    import matplotlib.pyplot as plt 
+    import statsmodels.api as sm
+    from statsmodels.graphics import tsaplots
 
-plt.figure(1)
-tsaplots.plot_acf(M,lags = 100)
-plt.show()
+    # Denne funktion tager det klippede og blurrede billede og bestemmer autocorrelation for alle pixels.
+    # Nedenfor er testscript til hvordan autocorrelationsfunktionen fungerer for en enkelt linje. 
+
+    # ## En linje 
+    # linje = clip[1,:]
+    # sm.tsa.acf(linje,nlags = 100)
+    # fig = tsaplots.plot_acf(linje,lags = 100)
+    # plt.show()
+
+    ## Et helt billede
+    # nlags bestemmer hvor mange pixels der medtages, 0 regnes ikke med og der er 1999 lig 2000. 
+    M = np.zeros(2000)
+    for i, clips in enumerate(clipBlur):
+        auto = sm.tsa.acf(clips,nlags = 1999)
+        M = M + auto
+        if i %10 == 0: 
+            print(i)
+    M = 1/2000*M
+
+    # lags bestemmer hvor mange punkter der plottes 
+    plt.figure(1)   
+    tsaplots.plot_acf(M,lags = 1999)
+    plt.show()
+    return M
+
+
 
 # x1 = np.arange(0, 100)
 # y1 = M[0,100]
 # plt.plot(x1,y1)
 # plt.show()
-=======
-## En linje 
-linje = clip[1,:]
-sm.tsa.acf(linje,nlags = 1999)
-fig = tsaplots.plot_acf(linje,lags = 1999)
-plt.show()
-
-## Et helt billede
-M = np.array([])
-for i, clips in enumerate(clip):
-    auto = sm.tsa.acf(clips,nlags = 1999)
-    M = M + auto
-print(np.size(M))
->>>>>>> Stashed changes
