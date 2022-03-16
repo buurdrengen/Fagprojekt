f6a5573e0295e4810e1f8e5c02ab2ee5cac2e279
99mjwes <s203965@student.dtu.dk>
2022-03-16 16:46:11 +0100

    Plot Update

--- plot_acf.py
+++ plot_acf.py
@@ -19,16 +19,17 @@ def plot_acf(acf, lags, n=1):
     plt.show()
 
 
-def lsm(x,y,m=[1, -1]):
+def lsm(x,y, m=[1, -1, 0.1], niter=20):
     """
     Least square method
     """
     y = y[:, np.newaxis]
 
-    for i in range(5):
+    for i in range(niter):
         d1 = dfd1(m,x)
         d2 = dfd2(m,x)
-        G = np.transpose(np.vstack((d1,d2)))
+        d3 = dfd3(m,x)
+        G = np.transpose(np.vstack((d1,d2,d3)))
         yz = y - func(m,x)[:,np.newaxis]
         delta = np.linalg.pinv(G).dot(yz) #magic
         res = np.dot(np.transpose(delta)[0],delta)
@@ -42,10 +43,13 @@ def lsm(x,y,m=[1, -1]):
 
 
 def func(m,x):
-    return m[0]*np.exp(m[1]*x) # + m[2]*np.exp(m[3]*x)
+    return m[0]*np.exp(m[1]*x) + m[2]
 
 def dfd1(m,x):
     return np.exp(m[1]*x)
 
 def dfd2(m,x):
     return m[0]*m[1]*np.exp(m[1]*x)
+
+def dfd3(m,x):
+    return np.ones(np.size(x))
