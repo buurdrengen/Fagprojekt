412aa1b35fd4d15f612b0098af4520687d117578
buurdrengen <s203947@student.dtu.dk>
2022-03-16 08:01:27 +0100

    autocofunc update

--- autocofunc.py
+++ autocofunc.py
@@ -12,8 +12,13 @@ threshold = 0.5
 
 image = skimage.io.imread(fname = "20200220_221515.jpg")
 clip = skimage.color.rgb2gray(image[y-margin:y+margin, x-margin:x+margin])
+<<<<<<< Updated upstream
 # plt.imshow(clip,cmap = 'gray')
 # plt.show()
+=======
+plt.imshow(clip,cmap = 'gray')
+plt.show()
+>>>>>>> Stashed changes
 
 clip[clip > threshold] = 0
 N = 2*margin
@@ -21,6 +26,7 @@ N = 2*margin
 horizontal = np.sum(clip,axis = 0)/N
 vertical = np.sum(clip,axis = 1)/N
 
+<<<<<<< Updated upstream
 # from clipBlur import clipBlur
 # def autoCor()
 
@@ -47,3 +53,17 @@ plt.show()
 # y1 = M[0,100]
 # plt.plot(x1,y1)
 # plt.show()
+=======
+## En linje 
+linje = clip[1,:]
+sm.tsa.acf(linje,nlags = 1999)
+fig = tsaplots.plot_acf(linje,lags = 1999)
+plt.show()
+
+## Et helt billede
+M = np.array([])
+for i, clips in enumerate(clip):
+    auto = sm.tsa.acf(clips,nlags = 1999)
+    M = M + auto
+print(np.size(M))
+>>>>>>> Stashed changes
