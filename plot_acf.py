543f7ef455fe94350d31be7a2ed98613431f6ee6
99mjwes <s203965@student.dtu.dk>
2022-03-23 14:21:02 +0100

    Faulty plot

--- plot_acf.py
+++ plot_acf.py
@@ -19,9 +19,6 @@ def plot_acf(acf, lags, n=1, conversion=90/2000, niter=20, func=1, saveas = None
     elif func == 2:
         fy = func2(m,x)
         plotlabel = r'$c_2 \exp(k_2 x^2) + c_1 \exp(k_1 x) + c_0$'
-    elif func == 3:
-        fy = func3(m,x)
-        plotlabel = r'$c_1 \exp(k_1 x^2)  + c_0$'
     
     #print(m)
     plt.figure()
@@ -37,8 +34,7 @@ def plot_acf(acf, lags, n=1, conversion=90/2000, niter=20, func=1, saveas = None
         fname = str("plotimg/" + saveas)
         #print(fname)
         plt.savefig(fname,dpi=300,format="png")
-    else:
-        plt.show()
+        plt.show(block=False)
     plt.close()
 
     rvs = np.cumsum(y)
@@ -53,44 +49,34 @@ def plot_acf(acf, lags, n=1, conversion=90/2000, niter=20, func=1, saveas = None
     print(f"Statistic is {stat:.04f} compared to {test:.04f}")
     print(f"p-value is {pval:.04f}")
 
-def lsm(x,y, m=[0.1, 1, -1, 1, -1], niter=50, func=1):
+def lsm(x,y, m=[0.1, 1, -1], niter=50, func=1):
     """
     Least square method
     """
     y = y[:, np.newaxis]
-    if func in [1,3]: m = m[0:3]
 
     for i in range(niter):
 
-        if func == 2:
-            #dobbelt eksponentiel funktion exp(x^2) + exp(x) + a0
+        if func == 1:
+            # eksponentiel funktion m1*exp(m2*x) + m0
             d0 = dfd0(m,x)
-            d1 = dfd1(m,x)
-            d2 = dfd2(m,x)
-            d3 = dfd3(m,x)
-            d4 = dfd4(m,x)
-            G = np.transpose(np.vstack((d0,d1,d2,d3,d4)))
-            yz = y - func2(m,x)[:,np.newaxis]
-        elif func == 1:
-            # eksponentiel funktion exp(x) + a0
-            d0 = dfd0(m,x)
-            d1 = dfd1(m,x)
-            d2 = dfd2(m,x)
+            d1 = df1d1(m,x)
+            d2 = df1d2(m,x)
             G = np.transpose(np.vstack((d0,d1,d2)))
             yz = y - func1(m,x)[:,np.newaxis]
-        elif func == 3:
-            #eksponentiel funktion exp(x^2) + a0
+        elif func == 2:
+            #Gaussisk funktion exp(x^2) + a0
             d0 = dfd0(m,x)
-            d1 = dfd3(np.array([m[0],0,0,m[1],m[2]]),x)
-            d2 = dfd4(np.array([m[0],0,0,m[1],m[2]]),x)
+            d1 = df2d1(m,x)
+            d2 = df2d2(m,x)
             G = np.transpose(np.vstack((d0,d1,d2)))
-            yz = y - func3(m,x)[:,np.newaxis]
+            yz = y - func2(m,x)[:,np.newaxis]
         
         delta = np.linalg.lstsq(G,yz,rcond=None)[0]
         #print(np.shape(delta))
         m = m + np.transpose(delta)[0]
         res = np.transpose(delta).dot(delta)
-        #print(f"Residuals for {i}: {res}")
+        print(f"Residuals for {i}: {res}")
         if res < 1e-8:
             break
         
@@ -101,27 +87,24 @@ def func1(m,x):
     return m[0] + m[1]*np.exp(m[2]*x)
 
 def func2(m,x):
-    return m[0] + m[1]*np.exp(m[2]*x) + m[3]*np.exp(m[4]*x**2)
+    return m[0] + 1/(m[1]*np.sqrt(2*np.pi)) * np.exp(-1/2 * ((x-m[2])/m[1])**2)
 
-def func3(m,x):
-    return m[0] + m[1]*np.exp(m[2]*x**2)
 
 
 def dfd0(m,x):
     return np.ones(np.size(x))
 
-def dfd1(m,x):
+def df1d1(m,x):
     return np.exp(m[1]*x)
 
-def dfd2(m,x):
-    return m[1]*m[2]*np.exp(m[2]*x)
+def df1d2(m,x):
+    return m[0]*m[1]*np.exp(m[1]*x)
 
-def dfd3(m,x):
-    return np.exp(m[4]*x**2)
+def df2d1(m,x):
+    return (x + m[1] -m[2])*(x - m[1] -m[2])/(m[1]**3) * func2([0,m[1],m[2]],x)
 
-def dfd4(m,x):
-    return m[3]*m[4]*np.exp(m[4]*x**2)
-
+def df2d2(m,x):
+    return (x-m[2])/(m[1]**2) * func2([0,m[1],m[2]],x)
 
 
 
