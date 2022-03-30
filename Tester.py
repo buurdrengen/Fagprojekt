6e6f13f17efe8c3c0ed33990d35f0c957bf96de6
99mjwes <s203965@student.dtu.dk>
2022-03-30 09:58:29 +0200

    Fixed Plots

--- Tester.py
+++ Tester.py
@@ -4,8 +4,14 @@ from clipBlur import clipBlur
 from autocofunc import autoCor
 from acf import acf
 #from string import rfind
+from plot_acf import func2
+import matplotlib.pyplot as plt
 
-# acf("thin_slices/firstyearice/southerntransect/20200201_145719.jpg", plot=True, plotfunc=1)
-# acf("thin_slices/firstyearice/southerntransect/20200201_145739.jpg", plot=True, plotfunc=1)
+acf("thin_slices/firstyearice/southerntransect/20200201_145719.jpg", plot=True, plotfunc=2)
+acf("thin_slices/firstyearice/southerntransect/20200201_145739.jpg", plot=True, plotfunc=2)
 
+plt.figure(10)
+xx = np.arange(-3,3,0.05)
+plt.plot(xx,func2([0,1,0,1],xx),'b-')
+plt.show()
 print(int(6.9))
--- plot_acf.py
+++ plot_acf.py
@@ -11,15 +11,20 @@ def plot_acf(acf, lags, n=1, conversion=90/2000, niter=20, func=1, saveas = None
     x = np.arange(lags)
     y = acf[x]
     x = x*conversion
-    m = lsm(x,y, niter=niter, func=func)
+    if func == 1:
+        m0 = [0.1, 1, -1]
+    elif func == 2:
+        m0 = [0.1,1,0.1,1]
+    
+    m = lsm(x, y, m=m0, niter=niter, func=func)
 
     if func == 1:
         fy = func1(m,x)
         plotlabel = r'$c_1 \exp(k_1 x) + c_0$'
     elif func == 2:
         fy = func2(m,x)
-        plotlabel = r'$c_2 \exp(k_2 x^2) + c_1 \exp(k_1 x) + c_0$'
-    
+        plotlabel = r'$c_1 / \sqrt{2 \pi \sigma^2} \cdot \exp\left(-\frac{\left(x - \mu \right)^2}{2 \sigma}\right) + c_0$'
+
     #print(m)
     plt.figure()
     plt.plot(x,y,'bo',label="ACF")
@@ -35,7 +40,6 @@ def plot_acf(acf, lags, n=1, conversion=90/2000, niter=20, func=1, saveas = None
         #print(fname)
         plt.savefig(fname,dpi=300,format="png")
         plt.show(block=False)
-    plt.close()
 
     rvs = np.cumsum(y)
     cdf = np.cumsum(fy)
@@ -49,34 +53,40 @@ def plot_acf(acf, lags, n=1, conversion=90/2000, niter=20, func=1, saveas = None
     print(f"Statistic is {stat:.04f} compared to {test:.04f}")
     print(f"p-value is {pval:.04f}")
 
+#---------------------------------------------------------------
+
+
 def lsm(x,y, m=[0.1, 1, -1], niter=50, func=1):
     """
     Least square method
     """
-    y = y[:, np.newaxis]
+    # Omdanner til søjlevektorer
+    y = np.hstack([np.flip(y),y])[:,np.newaxis]
+    x = np.hstack([np.flip(-x),x])[:,np.newaxis]
 
-    for i in range(niter):
+    #Itererer
+    for i in range(niter): 
 
         if func == 1:
             # eksponentiel funktion m1*exp(m2*x) + m0
             d0 = dfd0(m,x)
             d1 = df1d1(m,x)
             d2 = df1d2(m,x)
-            G = np.transpose(np.vstack((d0,d1,d2)))
-            yz = y - func1(m,x)[:,np.newaxis]
+            G = np.hstack((d0,d1,d2))
+            yz = y - func1(m,x)
         elif func == 2:
             #Gaussisk funktion exp(x^2) + a0
             d0 = dfd0(m,x)
             d1 = df2d1(m,x)
             d2 = df2d2(m,x)
-            G = np.transpose(np.vstack((d0,d1,d2)))
-            yz = y - func2(m,x)[:,np.newaxis]
+            d3 = df2d3(m,x)
+            G = np.hstack((d0,d1,d2,d3))
+            yz = y - func2(m,x)
         
-        delta = np.linalg.lstsq(G,yz,rcond=None)[0]
-        #print(np.shape(delta))
+        delta = np.linalg.lstsq(G,yz,rcond=None)[0] #Magi
         m = m + np.transpose(delta)[0]
         res = np.transpose(delta).dot(delta)[0][0]
-        #print(f"Residuals for {i}: {res}")
+        print(f"Residuals for {i}: {res}")
         if res < 1e-8:
             break
         
@@ -84,16 +94,18 @@ def lsm(x,y, m=[0.1, 1, -1], niter=50, func=1):
     return m
 
 
+#--------------------------------------------------------------
+
 def func1(m,x):
     return m[0] + m[1]*np.exp(m[2]*x)
 
 def func2(m,x):
-    return m[0] + 1/(m[1]*np.sqrt(2*np.pi)) * np.exp(-1/2 * ((x-m[2])/m[1])**2)
+    return m[0] + (m[3]/(m[1]*np.sqrt(2*np.pi))) * np.exp(-0.5 * ((x-m[2])**2) / (m[1]**2))
 
+#--------------------------------------------------------------
 
-
-def dfd0(m,x):
-    return np.ones(np.size(x))
+def dfd0(m,x): #c_0
+    return np.ones(np.size(x))[:,np.newaxis]
 
 def df1d1(m,x):
     return np.exp(m[2]*x)
@@ -101,11 +113,15 @@ def df1d1(m,x):
 def df1d2(m,x):
     return m[0]*m[1]*np.exp(m[2]*x)
 
-def df2d1(m,x):
-    return ((x - m[2])**2 - m[1]**2)/(m[1]**3) * func2([0,m[1],m[2]],x)
+def df2d1(m,x): #sigma
+    return (((x - m[2])**2 - m[1]**2)/(m[1]**3)) * func2([0,m[1],m[2],m[3]],x)
 
-def df2d2(m,x):
-    return (x-m[2])/(m[1]**2) * func2([0,m[1],m[2]],x)
+def df2d2(m,x): #mu
+    return ((x-m[2])/(m[1]**2)) * func2([0,m[1],m[2],m[3]],x)
+
+def df2d3(m,x): #C_1
+    return func2([0,m[1],m[2],1],x)
+
 
 
 
