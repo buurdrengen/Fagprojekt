e6d3c133383f91dce9193d17fd6dae0a6b5c5a05
99mjwes <s203965@student.dtu.dk>
2022-06-03 10:54:27 +0200

    dk

--- plot_threshold.py
+++ plot_threshold.py
@@ -6,12 +6,12 @@ from clipBlur import clipBlur
 
 
 if __name__ == "__main__":
-    fname = "thin_slices/meltponds/meltpond_kuka/Meltpond_kuka_9cm_top.jpg"
+    fname = "thin_slices/meltponds/meltpond_kuka/20200122_231339.jpg"
     fTypes = np.array(['Empirical','Exponential','Gaussian','Exp Root'])
     fit = np.array([0])
 
 
-    clip, blurredClip = clipBlur(fname, x=1800, y=2350, marginX=1350, marginY=1250, sigma=0.25)
+    clip, blurredClip = clipBlur(fname, x=1825, y=2150, marginX=1475, marginY=1150, sigma=0.25)
 
     plt.figure()
     plt.imshow(clip, cmap = 'gray')
@@ -35,16 +35,21 @@ if __name__ == "__main__":
     s1[s1 == 0] = 10
     s2[s2 == 0] = 10
     s3[s3 == 0] = 10
-    x = x*255
+    x = x
     plt.grid(True)
     plt.plot(x,s1,'r-',label='Section 1')
     plt.plot(x,s2,'b-',label='Section 2')
     plt.plot(x,s3,'k-',label='Section 3')
-    plt.xlabel("Brightness Threshold [8-bit value]")
+    plt.xlabel("Brightness Threshold [Normalized 8-bit value]")
     plt.ylabel("ACL [mm]")
     plt.title("Autocorrelation Length")
     plt.legend()
-    plt.xlim([-0.02*255,1.02*255])
+    plt.xlim([-0.02,1.02])
     plt.ylim([-0.04,2.04])
+
+    saveas = fname.split('/')[-1].split('.')[0]
+    savename = str("threshold_images/" + saveas + ".png")
+    #print(fname)
+    plt.savefig(savename,dpi=300,format="png")
     plt.show(block = True)
     plt.close()
