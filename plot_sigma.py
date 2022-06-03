67088757e6f2f2dd5fb6c53c8d0d545b0176aab7
99mjwes <s203965@student.dtu.dk>
2022-06-03 12:55:25 +0200

    sd

--- plot_sigma.py
+++ plot_sigma.py
@@ -17,13 +17,15 @@ if __name__ == "__main__":
     plt.imshow(clip, cmap = 'gray')
     plt.show()
     plt.close()
+    threshold = 0.6
 
-    x = np.arange(1,0.005,-0.005)
+    x = np.arange(0.1,4.05,0.05)
     s1 = np.zeros(np.size(x))
     s2 = np.zeros(np.size(x))
     s3 = np.zeros(np.size(x))
     for idx, i in enumerate(x):
-        clip[blurredClip > i] = 0
+        clip, blurredClip = clipBlur(fname, x=1825, y=2150, marginX=1475, marginY=1150, sigma=i)
+        clip[blurredClip > threshold] = 0
 
         auflength, functype, plotdata = acf(clip, lags = 200, conversion = 0.03214285714285714, plot = False, plotfunc = fit, ip=40, plotname="Testplot")
         s1[idx] = auflength[0]
@@ -31,24 +33,25 @@ if __name__ == "__main__":
         s3[idx] = auflength[2]
         print(auflength)
 
+    yscale = np.max([np.max(s1),np.max(s2),np.max(s3)])*1.5
+
     plt.figure()
     s1[s1 == 0] = 10
     s2[s2 == 0] = 10
     s3[s3 == 0] = 10
-    x = x
     plt.grid(True)
     plt.plot(x,s1,'r-',label='Section 1')
     plt.plot(x,s2,'b-',label='Section 2')
     plt.plot(x,s3,'k-',label='Section 3')
-    plt.xlabel("Brightness Threshold [Normalized 8-bit value]")
+    plt.xlabel("Standard Deviation")
     plt.ylabel("ACL [mm]")
     plt.title("Autocorrelation Length")
     plt.legend()
-    plt.xlim([-0.02,1.02])
-    plt.ylim([-0.04,2.04])
+    plt.xlim([-0.08,4.08])
+    plt.ylim([-0.02*yscale,1.02*yscale])
 
     saveas = fname.split('/')[-1].split('.')[0]
-    savename = str("threshold_images/" + saveas + ".png")
+    savename = str("sigma_images/" + saveas + ".png")
     #print(fname)
     plt.savefig(savename,dpi=300,format="png")
     plt.show(block = True)
