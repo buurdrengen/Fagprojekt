bf2451e362aaf20b6ab3e80ecc3c73c4fb391f2c
viktis1 <isaksenviktor@gmail.com>
2022-06-13 15:15:04 +0200

    s

--- nonTranposed.py
+++ nonTranposed.py
@@ -24,75 +24,75 @@ variables = [1,1,1,1,1,1,1,[1,1,1],['','','']]
 saveFile = [[1.0,1.0,1.0], 1.0, [1.0,1.0,1.0], [1.0,1.0,1.0], [1.0,1.0,1.0], [1.0, 1.0, 1.0], 1.0, 1.0]
 
 
-# for filename in os.listdir(files):
-#     with open(os.path.join(files, filename), 'r') as f: # open in readonly mode
-#         # print(np.arange(len(variables)))
+for filename in os.listdir(files):
+    with open(os.path.join(files, filename), 'r') as f: # open in readonly mode
+        # print(np.arange(len(variables)))
         
-#         for i in np.arange(len(variables)):
-#             variables[i] = f.readline()
-#             variables[i] = variables[i][0:-1]
+        for i in np.arange(len(variables)):
+            variables[i] = f.readline()
+            variables[i] = variables[i][0:-1]
     
-#     print('Processing file ' + filename[0:-4] + '...')
-#     # Now I find the fileplacement of the files 
-#     filenameM = images + '/' + filename[0:-4] + '.jpg'
-#     filenameSave = 'variables_nonT/' + filename
-#     imageSave = 'mod_image/' + filename[0:-4] + '_mod.jpg'
+    print('Processing file ' + filename[0:-4] + '...')
+    # Now I find the fileplacement of the files 
+    filenameM = images + '/' + filename[0:-4] + '.jpg'
+    filenameSave = 'variables_nonT/' + filename
+    imageSave = 'mod_image/' + filename[0:-4] + '_mod.jpg'
 
 
-#     yMiddle = int(variables[0])
-#     xMiddle = int(variables[1])
-#     marginY = int(variables[2])
-#     marginX = int(variables[3])
-#     [conversion, blur, threshold] = np.float64(variables[4:7])
+    yMiddle = int(variables[0])
+    xMiddle = int(variables[1])
+    marginY = int(variables[2])
+    marginX = int(variables[3])
+    [conversion, blur, threshold] = np.float64(variables[4:7])
 
-#     ## Get the autocorrelation lengths.
-#     image = skimage.io.imread(fname=filenameM, as_gray=True)
+    ## Get the autocorrelation lengths.
+    image = skimage.io.imread(fname=filenameM, as_gray=True)
 
-#     clip, blurredClip = clipBlur(filenameM, xMiddle, yMiddle, marginX, marginY, sigma = blur)
-#     rawclip = np.copy(clip)
-#     clip[blurredClip > threshold] = 0
+    clip, blurredClip = clipBlur(filenameM, xMiddle, yMiddle, marginX, marginY, sigma = blur)
+    rawclip = np.copy(clip)
+    clip[blurredClip > threshold] = 0
 
-#     num_pix = blurredClip < threshold
-#     n = np.shape(num_pix)[0]
-#     blox = np.int32(np.round(np.linspace(0,2*marginY,4)))
-#     rho = np.empty(3)
-#     for i in range(3):
-#         section = num_pix[blox[i]:blox[i+1],:]
-#         rho[i] = np.sum(section) / np.size(section)
-#     # print(rho)
+    num_pix = blurredClip < threshold
+    n = np.shape(num_pix)[0]
+    blox = np.int32(np.round(np.linspace(0,2*marginY,4)))
+    rho = np.empty(3)
+    for i in range(3):
+        section = num_pix[blox[i]:blox[i+1],:]
+        rho[i] = np.sum(section) / np.size(section)
+    # print(rho)
     
-#     auflength  = np.empty(3)
-#     uncertainty = np.empty(3)
-#     RMSE = np.empty([3,4])
-#     kvalue = np.empty(3)
-#     xvalue = np.empty(3)
-#     auflength, uncertainty, funcType, plotdata, RMSE, kvalue, xvalue = acf(clip, lags = marginX-1, conversion = conversion, plot = False, plotfunc = fit)
+    auflength  = np.empty(3)
+    uncertainty = np.empty(3)
+    RMSE = np.empty([3,4])
+    kvalue = np.empty(3)
+    xvalue = np.empty(3)
+    auflength, uncertainty, funcType, plotdata, RMSE, kvalue, xvalue = acf(clip, lags = marginX-1, conversion = conversion, plot = False, plotfunc = fit)
 
     #-----------------------------------------------------------------
     # Plot - Kan udkommenteres ->
-    # funcTypes = np.array(["Exponential","Gaussian", "x-Power", "x-Exponential"])
-    # print(' -> Plotdata...')
-    # plot_acf2(auflength, funcTypes, plotdata, xmax = 2, block = False, sectors = 3, saveas = filename[0:-4], plotshow=False)
-    # print(' -> Threshold...')
-    # plot_threshold(clip=np.copy(rawclip), blurredClip=np.copy(blurredClip), conversion=conversion, saveas = filename[0:-4], plotshow = False)
-    # print(' -> Sigma...')
-    # plot_sigma(clip=rawclip, threshold=threshold, conversion=conversion, saveas = filename[0:-4], plotshow = False)
+    funcTypes = np.array(["Exponential","Gaussian", "x-Power", "x-Exponential"])
+    print(' -> Plotdata...')
+    plot_acf2(auflength, funcTypes, plotdata, xmax = 2, block = False, sectors = 3, saveas = filename[0:-4], plotshow=False)
+    print(' -> Threshold...')
+    plot_threshold(clip=np.copy(rawclip), blurredClip=np.copy(blurredClip), conversion=conversion, saveas = filename[0:-4], plotshow = False)
+    print(' -> Sigma...')
+    plot_sigma(clip=rawclip, threshold=threshold, conversion=conversion, saveas = filename[0:-4], plotshow = False)
     #-----------------------------------------------------------------
     
-    # variables = [yMiddle, xMiddle, marginY, marginX, conversion, blur, threshold, auflength[0], \
-    #     auflength[1], auflength[2], funcType]
+    variables = [yMiddle, xMiddle, marginY, marginX, conversion, blur, threshold, auflength[0], \
+        auflength[1], auflength[2], funcType]
     
-    # saveFile = [np.round(auflength,3), np.round(uncertainty,3), np.round(RMSE[:,0]*10**4,3), np.round(RMSE[:,1]*10**2,3), np.round(RMSE[:,2]*10**3,3), np.round(RMSE[:,3]*10**5,3), np.round(kvalue,3), np.round(xvalue,3), np.round(rho, 3)]
+    saveFile = [np.round(auflength,3), np.round(uncertainty,3), np.round(RMSE[:,0]*10**4,3), np.round(RMSE[:,1]*10**2,3), np.round(RMSE[:,2]*10**3,3), np.round(RMSE[:,3]*10**5,3), np.round(kvalue,3), np.round(xvalue,3), np.round(rho, 3)]
 
-    # print(RMSE)
-    # #print('')
+    print(RMSE)
+    #print('')
 
-    # np.savetxt(filenameSave, saveFile, delimiter=' ', newline = "\n", fmt = "%s")
+    np.savetxt(filenameSave, saveFile, delimiter=' ', newline = "\n", fmt = "%s")
 
 
-    # clip[blurredClip > threshold] = 1
-    # clip = np.uint8(clip*255)
-    # skimage.io.imsave(imageSave, clip)
+    clip[blurredClip > threshold] = 1
+    clip = np.uint8(clip*255)
+    skimage.io.imsave(imageSave, clip)
     
 
 #-----------------------------------------------------------------
